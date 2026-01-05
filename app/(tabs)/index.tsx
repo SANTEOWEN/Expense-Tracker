9ce0454cb0da1c606c467a56f0f6e28b576767c0
SANTEOWEN <owensante45@gmail.com>
2026-01-06 00:02:31 +0800

    Added some static UI stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,23 +6,23 @@ const index = () => {
   return (
     <>
     <View className='flex-col h-full gap-3'>
-        <View className='flex-1 bg-green-600 rounded-md mx-5 mt-10'>
+        <View className='flex-1 bg-[#2bb673] rounded-md mx-5 mt-10'>
             <View className='flex-1 flex-col gap-2'>
-                <View className='p-10 flex-col gap-3'>
-                    <Text className='text-white text-xl font-bold'>Total Balance</Text>
+                <View className='flex-1 px-10 flex-col justify-center gap-3'>
+                    <Text className='text-white text-2xl font-bold'>Total Balance</Text>
                     <Text className='text-white text-4xl'>$1,234.56</Text>
                 </View>
             </View>
 
             <View className='flex-1'>
-                <View className='flex-1 flex-row p-11 bg-orange-500'>
-                    <View className='rounded-sm bg-green-300'>
-                        <Text className='text-white text-xl font-bold'>Income</Text>
-                        <Text className='text-white text-2xl font-bold'>$1,234.56</Text>
+                <View className='flex-1 flex-row items-center justify-start gap-3 px-10'>
+                    <View className='flex-1 p-4 rounded-md bg-green-300'>
+                        <Text className='text-green-100 text-lg font-bold'>Income</Text>
+                        <Text className='text-white text-2xl'>$ 1,234</Text>
                     </View>
-                    <View className=''>
-                        <Text className='text-white text-xl font-bold'>Expenses</Text>
-                        <Text className='text-white text-2xl font-bold'>$1,234.56</Text>
+                    <View className='flex-1 p-4 rounded-md bg-green-300'>
+                        <Text className='text-green-100 text-lg font-bold'>Expenses</Text>
+                        <Text className='text-white text-2xl'>$ 1,234</Text>
                     </View>
                 </View>
             </View>
@@ -36,7 +36,7 @@ const index = () => {
             </View>
         </View>
 
-        <View className='flex-1 grow-[2] bg-blue-400 rounded-md'>
+        <View className='flex-1 grow-[2] rounded-md'>
 
         </View>
     </View>
