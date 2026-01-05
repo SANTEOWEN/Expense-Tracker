2cf8981486892da1229ecd9c4509c36f63bd6bb6
SANTEOWEN <owensante45@gmail.com>
2026-01-06 00:02:46 +0800

    Small Changes for it to test the page.

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const history = () => {
+const History = () => {
   return (
-    <View>
+    <View className='flex-1 justify-center items-center'>
       <Text>history</Text>
     </View>
   )
 }
 
-export default history
+export default History
