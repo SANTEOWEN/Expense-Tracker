a508ab88d80980020c5bdb6bf1debc3d38ee4489
SANTEOWEN <owensante45@gmail.com>
2026-01-06 00:02:08 +0800

    Added different Icons for the tab bar and styled it a bit.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,44 @@
 import { Tabs } from "expo-router";
+import { History, Home, SlidersVertical } from 'lucide-react-native';
 import React from 'react';
 
 export default function TabLayout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: 'Home' }} />
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: '#2bb673',
+            tabBarInactiveTintColor: '#556063',
+            tabBarStyle: {
+                borderTopWidth:1,
+                borderTopColor: '#2bb673',
+                height: 100,
+                paddingBottom: 10,
+                paddingTop: 10
+            },
+            tabBarLabelStyle: {
+                fontSize: 14,
+                fontWeight: '600'
+            },
+            headerShown: false
+
+        }}>
+            <Tabs.Screen name="index"
+            options={{
+                title: 'Home',
+                tabBarIcon: ({color, size}) => (<Home color={color} size={size} />)
+            }}/>
+
+            <Tabs.Screen name="history"
+            options={{
+                title: 'History',
+                tabBarIcon: ({color, size}) => (<History color={color} size={size} />)
+            }} />
+
+            <Tabs.Screen name="settings"
+            options={{
+                title: 'Settings',
+                tabBarIcon: ({color, size}) => (<SlidersVertical color={color} size={size} />)
+            }} />
+
         </Tabs>
     )
 }
