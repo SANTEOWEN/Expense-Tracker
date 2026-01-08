25471354e673d3935b251e4bf651fb7d4b721b67
SANTEOWEN <owensante45@gmail.com>
2026-01-09 00:59:36 +0800

    Added NavTheme provider for the whole layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,20 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css";
 
+import { useColorScheme } from "@/lib/useColorScheme";
+import { NAV_THEME } from '@/theme';
+import { ThemeProvider as NavThemeProvider } from "@react-navigation/native";
+
 export default function RootLayout() {
+  const {colorScheme, isDarkColorScheme} = useColorScheme();
+
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-  </SafeAreaProvider>
+    <NavThemeProvider value={NAV_THEME[colorScheme]}>
+        <SafeAreaProvider>
+            <Stack screenOptions={{headerShown: false}}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </SafeAreaProvider>
+  </NavThemeProvider>
   )
 }
