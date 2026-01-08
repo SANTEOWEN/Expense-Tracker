a7f0ad7843a19241daf876f17ba8441f59ab938f
SANTEOWEN <owensante45@gmail.com>
2026-01-09 00:59:10 +0800

    added dark mode options on the system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,65 @@
+const { hairlineWidth, platformSelect } = require("nativewind/theme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        border: withOpacity('border'),
+        input: withOpacity('input'),
+        ring: withOpacity('ring'),
+        background: withOpacity('background'),
+        foreground: withOpacity('foreground'),
+        primary: {
+          DEFAULT: withOpacity('primary'),
+          foreground: withOpacity('primary-foreground'),
+        },
+        secondary: {
+          DEFAULT: withOpacity('secondary'),
+          foreground: withOpacity('secondary-foreground'),
+        },
+        destructive: {
+          DEFAULT: withOpacity('destructive'),
+          foreground: withOpacity('destructive-foreground'),
+        },
+        muted: {
+          DEFAULT: withOpacity('muted'),
+          foreground: withOpacity('muted-foreground'),
+        },
+        accent: {
+          DEFAULT: withOpacity('accent'),
+          foreground: withOpacity('accent-foreground'),
+        },
+        popover: {
+          DEFAULT: withOpacity('popover'),
+          foreground: withOpacity('popover-foreground'),
+        },
+        card: {
+          DEFAULT: withOpacity('card'),
+          foreground: withOpacity('card-foreground'),
+        },
+      },
+      borderWidth: {
+        hairline: hairlineWidth(),
+      },
     },
   },
   plugins: [],
+}
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return platformSelect({
+        ios: `color-mix(in srgb, var(--${variableName}) ${opacityValue * 100}%, transparent)`,
+        android: `color-mix(in srgb, var(--android-${variableName}) ${opacityValue * 100}%, transparent)`,
+      });
+    }
+    return platformSelect({
+      ios: `var(--${variableName})`,
+      android: `var(--android-${variableName})`,
+    });
+  };
 }
