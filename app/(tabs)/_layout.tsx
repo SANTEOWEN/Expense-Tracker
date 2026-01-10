be415cf8b7395c8a61f5ef30aecce89e4622fba3
SANTEOWEN <owensante45@gmail.com>
2026-01-11 00:36:41 +0800

    Added customized TabBar component on tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,54 +1,27 @@
-import { useColorScheme } from "@/lib/useColorScheme";
-import { COLORS } from "@/theme/colors";
+import { TabBar } from "@/components/TabBar";
 import { Tabs } from "expo-router";
-import { History, Home, SlidersVertical } from 'lucide-react-native';
 import React from 'react';
-COLORS
 
 export default function TabLayout() {
-    const {colorScheme, isDarkColorScheme} = useColorScheme();
 
     return (
         <Tabs
+        tabBar={(props) => <TabBar {...props}/>}
         screenOptions={{
-            tabBarActiveTintColor: isDarkColorScheme ? COLORS.light.primary : COLORS.dark.primary,
-            tabBarInactiveTintColor: '#556063',
-            tabBarStyle: {
-                position: 'absolute',
-                bottom: 25,
-                left: 20,
-                right: 20,
-                elevation: 0,
-                height: 90,
-                borderRadius: 15,
-            },
-            tabBarLabelStyle: {
-                fontSize: 14,
-                fontWeight: '600'
-            },
-            headerShown: false,
-            tabBarPosition: 'bottom',
-            animation: 'shift',
+            headerShown: false
+        }}>
+            <Tabs.Screen
+            name="index"
+            options = {{title: 'Home'}}
+            />
 
-        }}
-        >
-            <Tabs.Screen name="index"
-            options={{
-                title: 'Home',
-                tabBarIcon: ({color, size}) => (<Home color={color} size={size} />)
-            }}/>
+            <Tabs.Screen
+            name="history"
+            options={{title: 'History'}}/>
 
-            <Tabs.Screen name="history"
-            options={{
-                title: 'History',
-                tabBarIcon: ({color, size}) => (<History color={color} size={size} />)
-            }} />
-
-            <Tabs.Screen name="settings"
-            options={{
-                title: 'Settings',
-                tabBarIcon: ({color, size}) => (<SlidersVertical color={color} size={size} />)
-            }} />
+            <Tabs.Screen
+            name="settings"
+            options={{title: 'Options'}}/>
 
         </Tabs>
     )
