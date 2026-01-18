6a480f4c3ff839d992a9e69c994aa0d7dfac2b56
SANTEOWEN <owensante45@gmail.com>
2026-01-18 23:55:55 +0800

    Renamed Most some stuff.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,21 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css";
 
-import { useColorScheme } from "@/lib/useColorScheme";
+import { useColorScheme } from "@/hooks/useColorScheme";
 import { NAV_THEME } from '@/theme';
 import { ThemeProvider as NavThemeProvider } from "@react-navigation/native";
+
 
 export default function RootLayout() {
   const {colorScheme, isDarkColorScheme} = useColorScheme();
 
   return (
-    <NavThemeProvider value={NAV_THEME[colorScheme]}>
-        <SafeAreaProvider>
-            <Stack screenOptions={{headerShown: false}}>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-        </SafeAreaProvider>
-  </NavThemeProvider>
+        <NavThemeProvider value={NAV_THEME[colorScheme]}>
+                <SafeAreaProvider>
+                        <Stack screenOptions={{headerShown: false}}>
+                            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                        </Stack>
+                </SafeAreaProvider>
+        </NavThemeProvider>
   )
 }
